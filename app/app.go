5d58078107f491d17d326e500f15007d2344c3f8
Pratik <mota.pratik@gmail.com>
2021-01-03 00:52:08 +0530

    gorilla mux intro

--- app/app.go
+++ app/app.go
@@ -1,15 +1,29 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Start func
 func Start() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/greet", greeting)
-	mux.HandleFunc("/customer", getAllCustomers)
+	router := mux.NewRouter()
+	router.HandleFunc("/greet", greeting).Methods(http.MethodGet)
+	router.HandleFunc("/customer", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customer", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customer/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post method")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/motapratik/BankingApp
 
 go 1.14
+
+require github.com/gorilla/mux v1.8.0
